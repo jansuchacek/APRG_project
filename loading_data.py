f4c03fc43f9bb607ac8cbaa883aa9fc36ff55919
Jan Suchacek <suchac03@seznam.cz>
2020-04-06 00:20:47 +0200

    Načtení dat, startu a cíle

--- loading_data.py
+++ loading_data.py
@@ -1,25 +1,61 @@
 import json
 
-with open('map_data_0.json') as map_data_0:
-  data = json.load(map_data_0)
 
-print(data)
+def loading_map():
+    """
+    Tato funkce načte mapu
+    :return: výstupem funkce bude seznam souřadnic
+    """
+    with open('map_data_0.json') as map_data_0:
+        data = json.load(map_data_0)
 
-
-map_name = data["map_name"]
-map_units = data["units"]
-#map_coordinates = data["object"][0]["coordinates"]
-map_shapes = data["object"]
-
-map_shapes_count = len(map_shapes)
-
-counter = 0
-points = []
-shape_names = []
-for shape in map_shapes:
-    points.append(shape["coordinates"])
-    shape_names.append(shape["name"])
-    counter += 1
+    #print(data)
 
 
-print(data.map_name)
+    map_name = data["map_name"]
+    map_units = data["units"]
+    #map_coordinates = data["object"][0]["coordinates"]
+    map_objects = data["object"]
+    map_objects_count = len(map_objects)
+
+    points = []
+    object_names = []
+    for object in map_objects:
+        points.append(object["coordinates"])
+        object_names.append(object["name"])
+
+
+    return points
+
+
+def load_start_and_end():
+    """
+    Tato funkce načte startovní a cílovou pozici
+    :return: Výstupem této funkce bude startovní a cílová poizce
+    """
+    with open('test_path.json') as test_path:
+        data = json.load(test_path)
+
+    #print(data)
+
+    starts_and_ends = data["path"]
+
+    starts = []
+    ends = []
+    for dct in starts_and_ends:
+        starts.append(dct["start"])
+        ends.append(dct["end"])
+
+    return starts, ends
+
+
+def main():
+    points = loading_map()
+    print("points:", points)
+    starts, ends = load_start_and_end()
+    print("starts", starts, "ends", ends)
+
+
+if __name__ == "__main__":
+    main()
+
