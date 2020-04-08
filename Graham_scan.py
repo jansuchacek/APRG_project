bc81e522b48d2d8d98dcc81acf6c499e68c2f52d
Samuel Sipula <samosipula@gmail.com>
2020-04-08 23:36:55 +0200

    Graham Scan - este nefunkcny

--- Graham_scan.py
+++ Graham_scan.py
@@ -5,7 +5,7 @@ import config
 import loading_data
 
 
-def scatter_plot(points, convex_hull=None):
+def display_points(points, convex_hull=None):
     for set_of_points in points:
         xs, ys = zip(*set_of_points)
         plt.scatter(xs, ys)
@@ -14,37 +14,37 @@ def scatter_plot(points, convex_hull=None):
         for i in range(1, len(convex_hull)+1):
             if i == len(convex_hull):
                 i = 0
-            c0 = convex_hull[i-1]
-            c1 = convex_hull[i]
-            plt.plot((c0[0], c1[0]), (c0[1], c1[1]), "r")
+            h0 = convex_hull[i-1]
+            h1 = convex_hull[i]
+            plt.plot((h0[0], h1[0]), (h0[1], h1[1]), "r")
     plt.show()
 
 
 def polar_angle(p0, p1=None):
     if p1 == None:
         p1 = anchor
-    y_span = p0[1] - p1[1]
-    x_span = p0[0] - p1[0]
-    return atan2(y_span, x_span)
+    y_line = p0[1] - p1[1]
+    x_line = p0[0] - p1[0]
+    return atan2(y_line, x_line)
 
 
 def get_distance(p0, p1=None):
     if p1 == None:
         p1 = anchor
-    y_span = p0[1] - p1[1]
-    x_span = p0[0] - p1[0]
-    return y_span**2 + x_span**2
+    y_line = p0[1] - p1[1]
+    x_line = p0[0] - p1[0]
+    return atan2(y_line, x_line)
 
 
-def det(p1,p2,p3):
+def determinant(p1,p2,p3):
     return (p2[0]-p1[0])*(p3[1]-p1[1]) \
                   -(p2[1]-p1[1])*(p3[0]-p1[0])
 
 
-def quicksort(a):
+def angle_sort(a):
     if len(a) <= 1:
         return a
-    smaller, equal, larger = [], [], []
+    smaller, equal, bigger = [], [], []
     piv_ang = polar_angle(a[randint(0, len(a)-1)])
     for pt in a:
         pt_ang = polar_angle(pt)
@@ -53,10 +53,10 @@ def quicksort(a):
         elif pt_ang == piv_ang:
             equal.append(pt)
         else:
-            larger.append(pt)
-    return quicksort(smaller) \
+            bigger.append(pt)
+    return angle_sort(smaller) \
             +sorted(equal, key=get_distance) \
-            +quicksort(larger)
+            +angle_sort(bigger)
 
 
 def graham_scan(points, show_progress=False):
@@ -70,16 +70,16 @@ def graham_scan(points, show_progress=False):
             if (y == sets[min_idx][1]) and (x < sets[min_idx][0]):
                 min_idx = i
         anchor = sets[min_idx]
-        sorted_points = quicksort(sets)
+        sorted_points = angle_sort(sets)
         del sorted_points[sorted_points.index(anchor)]
         hull = [anchor, sorted_points[0]]
         for s in sorted_points[1:]:
-            while det(hull[-2], hull[-1], s) <= 0:
+            while determinant(hull[-2], hull[-1], s) <= 0:
                 del hull[-1]
                 #if len(hull)<2: break
             hull.append(s)
             if show_progress:
-                scatter_plot(sets, hull)
+                display_points(sets, hull)
         return hull
 
 
@@ -88,7 +88,7 @@ def main():
     map1 = config.MAPS["map_1"]
     points = loading_data.loading_map(map1)
     graham_scan(points, True)
-    scatter_plot(points, graham_scan(points, True))
+    display_points(points, graham_scan(points, True))
 
 
 if __name__ == "__main__":
