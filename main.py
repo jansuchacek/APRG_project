8e6eb52030c6845288ccf819f6c9db6cf75b5a42
Jan Suchacek <suchac03@seznam.cz>
2020-05-01 13:28:25 +0200

    Hlavní funkce

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import Graham_scan
 from matplotlib import pyplot as plt
 import config
 import loading_data
+import data_display
 
 
 def main():
@@ -14,12 +15,23 @@ def main():
     map1 = config.MAPS["map_1"]
     points = loading_data.loading_map(map0)
 
+    #Zobrazení mapy:
+    data_display.draw_points(points)
+
+    #Počáteční a koncové body:
+    test_path = config.TEST_PATH["test_path"]
+    starts, ends = loading_data.load_start_and_end(test_path)
+    print("Pocatecni body:", starts)
+    print("Koncove body:", ends)
+
+    #Graham scan:
+    i = 1
     for sets in points:
+        print("Polygon cislo", i, ":", sets)
         Graham_scan.zobrazeni_bodu(sets, Graham_scan.graham_scan(sets))
+        i += 1
     return plt.show()
 
 
 if __name__ == "__main__":
     main()
-
-
